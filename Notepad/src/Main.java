6ed9a173b9ea93ecbe8fc168000194d4e907f517
jungeun-choi <jungeun5.choi@gmail.com>
2023-06-05 11:14:36 +0900

    Main - 메모장 진입

--- Notepad/src/Main.java
+++ Notepad/src/Main.java
@@ -1,4 +1,16 @@
 package Notepad.src;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+
+        // 객체 생성
+        Scanner sc = new Scanner(System.in);
+        MemoList memoList = new MemoList();
+        NotepadApp notepadApp = new NotepadApp(sc, memoList);
+
+        // 시작
+        notepadApp.startNotepadApp();
+    }
 }
