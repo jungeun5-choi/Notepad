d42e8bd996dd80813a9afea09f14862745353683
Ahn Jeongmin <symbol9550@gmail.com>
2023-06-05 10:40:55 +0900

    fix count

--- Notepad/src/MemoList.java
+++ Notepad/src/MemoList.java
@@ -8,7 +8,7 @@ public class MemoList {
     LinkedList<MemoVO> memoList = new LinkedList<>(); // 메모를 저장하는 리스트
     Scanner sc = new Scanner(System.in);
     MemoVO vo = new MemoVO();
-    private int i = 1; // 첫번째 메모를 1번으로 표현하기 위한 선언
+    private int count = 1; // 첫번째 메모를 1번으로 표현하기 위한 선언
 
     public void Insert() {
         LocalDateTime time = LocalDateTime.now();   // ISO 8601 형식의 시스템상의 현재 시간
@@ -20,7 +20,7 @@ public class MemoList {
         String memo = sc.nextLine();
 
         MemoVO vo = new MemoVO();
-        vo.setIdx(i++); // 번호
+        vo.setIdx(count++); // 번호
         vo.setName(name);   // 이름
         vo.setPassword(password);   // 비밀번호
         vo.setMemo(memo);   // 메모 내용
@@ -71,6 +71,7 @@ public class MemoList {
                     for(int i = memoList.size()-1;i>=0;i--){
                         memoList.get(i).setIdx(i+1);
                     }
+                    count--;
                 } else{                                                 // 입력한 pass와 getPassword로 불러온 값이 다르면 진행
                     System.out.println("비밀번호가 틀렸습니다.");
                 }
